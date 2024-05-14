e8d1603196e8e9f017173fd1bc27dd53101cbffc
Ross Karchner <ross@karchner.com>
2024-05-14 12:54:17 -0400

    add text labels to progress bar

--- scenes/hud.gd
+++ scenes/hud.gd
@@ -1,9 +1,15 @@
 extends Control
 
 @onready var bar = %ProgressBar
+@onready var progress_labels = %ProgressLabels
+@onready var day_label = %DayLabel
+@onready var night_label = %NightLabel
 
 func _ready():
 	CountManager.score_changed.connect(update_scores)
 	
 func update_scores(day_score):
 	bar.value = day_score
+	day_label.text = str(day_score)
+	night_label.text = str(556-day_score)
+	progress_labels.position.x = (day_score/556.0) * 600
