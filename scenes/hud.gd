483d66dfe0195afa2b6e990070c58e35bbab3722
Ross M Karchner <ross@karchner.com>
2024-05-14 06:15:20 -0400

    Update hud.gd
    remove old comments

--- scenes/hud.gd
+++ scenes/hud.gd
@@ -6,7 +6,5 @@ func _ready():
 	CountManager.score_changed.connect(update_scores)
 	
 func update_scores(day_score, night_score):
-	#day_label.text=str(day_score)
-	#night_label.text = str(night_score)
 	assert(day_score+night_score == 576)
 	bar.value = day_score
